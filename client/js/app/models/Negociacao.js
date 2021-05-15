5468dafc869eeb147fca40107b21d7a759efa4d8
marlondck <15050029+marlondck@users.noreply.github.com>
2021-05-15 12:54:54 -0400

    intro a classes e private fields

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,27 @@
+class Negociacao {
+  #data;
+  #quantidade;
+  #valor;
+
+  constructor (data, quantidade, valor) {
+    this.#data = data;
+    this.#quantidade = quantidade;
+    this.#valor = valor;
+  }
+
+  getData() {
+    return this.#data;
+  }
+
+  getQuantidade() {
+    return this.#quantidade;
+  }
+
+  getValor() {
+    return this.#valor
+  }
+  
+  getVolume () {
+    return this.#quantidade * this.#valor
+  }
+}
